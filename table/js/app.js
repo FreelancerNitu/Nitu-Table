55b728ac4119534122000eff028b214bbcc93e51
NA Shree Nitu <79525128+FreelancerNitu@users.noreply.github.com>
2021-10-29 00:00:31 +0600

    Added here only HTML, and Css for this table

---
+++ table/js/app.js
@@ -0,0 +1,15 @@
+// var h = document.createElement('h3')
+// var myvalue = document.createTextNode('Hello World')
+// h.appendChild(myvalue)
+
+// document.querySelector('h3').appendChild(h);
+
+
+// Add item
+
+function addItem(){
+  var input = document.getElementById('input');
+  var myValue = input.value;
+  ul = document.getElementById('list');
+}
+addItem();
